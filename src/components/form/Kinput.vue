5d2dbaae1f5d51edcbc08678ea65a7df475a2f4c
MR.Wang <15268638853@163.com>
2020-01-05 00:45:20 +0800

    封装form表单

---
+++ src/components/form/Kinput.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <input type="text" :value="value" @input="onInput" />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+    props: {
+      value: {
+        type: String,
+        default: ''
+      },
+      type: {
+        type: String,
+        default: 'text'
+      }
+    },
+    created() {},
+    mounted() {},
+    methods: {
+        onInput(e){
+            this.$emit("input",e.target.value)
+            this.$parent.validator()
+        }
+    }
+};
+</script>
+
+<style scoped lang="less">
+</style>
---
+++ src/components/form/index.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <h3>KForm表单</h3>
+        <hr />
+        <k-form :model="userInfo" :rules="rules" ref="form">
+            <k-form-item label="用户名" prop="username">
+                <k-input v-model="userInfo.username"></k-input>
+            </k-form-item>
+            <k-form-item label="密码" prop="password">
+                <k-input v-model="userInfo.password"></k-input>
+            </k-form-item>
+        </k-form>
+        <button @click="onClick">提交</button>
+        {{userInfo}}
+    </div>
+</template>
+
+<script>
+import KForm from "./form";
+import KInput from "./Kinput";
+import KFormItem from "./KFormItem";
+export default {
+    provide() {
+        return {
+            form: this
+        };
+    },
+    components: {
+        KForm,
+        KInput,
+        KFormItem
+    },
+    data() {
+        return {
+            userInfo: {
+                username: "tom",
+                password: ""
+            },
+            rules: {
+                username: [{ required: true, message: "请输入用户名称" }],
+                password: [{ required: true, message: "请输入密码" }]
+            }
+        };
+    },
+
+    created() {},
+    mounted() {},
+    methods: {
+        onClick() {
+            this.$refs.form.validator(boolean => {
+                if (boolean) {
+                    console.log("chenggong");
+                } else {
+                    console.log("失败");
+                }
+            });
+        }
+    }
+};
+</script>
+
+<style scoped lang="less">
+</style>
